b0f2859939350086736e0044d6b1c43e021a1f4f
Edson <edsonaugusto2006@gmail.com>
2026-08-13 15:53:56 -0300

    Setting up DependencyInjection

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
@@ -1,16 +1,25 @@
 ﻿using Mapster;
 using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Domain.Security.PasswordHashing;
 using MyRecipeBook.Exception.ExceptionsBase;
 
 namespace MyRecipeBook.Application.UseCases.User.Register;
 
-public class RegisterUserAccountUseCase
+public class RegisterUserAccountUseCase : IRegisterUserAccountUseCase
 {
+    private readonly IPasswordHasher _passwordHasher;
+    public RegisterUserAccountUseCase(IPasswordHasher passwordHasher)
+    {
+        _passwordHasher = passwordHasher;
+    }
+
     public void Execute(RequestRegisterUserAccountJson request)
     {
         ValidateAndThrowOnFailures(request);
 
         var user = request.Adapt<Domain.Entities.User>(); // Mapeando os dados do request para a Entidade User. (Usando a lib "Mapster"). Funciona pois os nomes dos atributos do objeto da request e os nomes da entidade sao os mesmos.
+
+        user.Password = _passwordHasher.HashPassword(request.Password);    
     }
 
 
---
+++ src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using Konscious.Security.Cryptography;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Security.PasswordHashing;
+using MyRecipeBook.Infrastructure.Security.PasswordHashing;
+
+namespace MyRecipeBook.Infrastructure;
+
+public class DependencyInjectionExtension
+{
+    public void AddInfrastructure(IServiceCollection services)
+    {
+        services.AddScoped<IPasswordHasher, Argon2PasswordHasher>(); //registra o hasher de senha no serviço de injeçao de dependencia
+                                                                     // "Quando alguem solicitar um objeto que implementa IPasswordHasher vc devolve uma instancia da classe Argon2PasswordHasher"
+    }
+}
