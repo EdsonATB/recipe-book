df06863ec28ea97881998322681f945a42fda535
Edson <edsonaugusto2006@gmail.com>
2026-08-14 15:30:11 -0300

    Set up DbContext (Entity Framework)

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/MyRecipeBookDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
+
+namespace MyRecipeBook.Infrastructure.DataAccess;
+
+internal class MyRecipeBookDbContext : DbContext
+{
+    public MyRecipeBookDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { } //repassa o parametro para a DbContext
+    
+    public DbSet<User> Users { get; set; } //O nome dessa propriedade tem que ser o mesmo nome  da tabela do db
+}
