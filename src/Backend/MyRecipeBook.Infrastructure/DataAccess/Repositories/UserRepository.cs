54ee1020f6898d425eea113f4607156e98240310
Edson <edsonaugusto2006@gmail.com>
2026-08-17 17:48:13 -0300

    Add interface for read only operations for the user repository

--- src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/Repositories/UserRepository.cs
@@ -1,9 +1,10 @@
-﻿using MyRecipeBook.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Domain.Entities;
 using MyRecipeBook.Domain.Repositories.User;
 
 namespace MyRecipeBook.Infrastructure.DataAccess.Repositories;
 
-internal sealed class UserRepository : IUserWriteOnlyRepository
+internal sealed class UserRepository : IUserWriteOnlyRepository, IUserReadOnlyRepository
 {
     private readonly MyRecipeBookDbContext _dbContext;
     public UserRepository(MyRecipeBookDbContext dbContext)
@@ -12,5 +13,6 @@ internal sealed class UserRepository : IUserWriteOnlyRepository
     }
 
     public async Task Add(User user) => await _dbContext.Users.AddAsync(user);
-    
+
+    public async Task<bool> ExistActiveUserWithEmail(string email) => await _dbContext.Users.AnyAsync(user => user.Active && user.Email.Equals(email));
 }
