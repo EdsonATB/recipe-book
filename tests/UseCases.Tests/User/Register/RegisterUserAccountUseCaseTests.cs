fc266bb9934b0d848b1779ac59e04679df86c15a
Edson <edsonaugusto2006@gmail.com>
2026-08-31 17:45:00 -0300

    Add Success unit test for RegisterUserAccountUseCase

---
+++ tests/UseCases.Tests/User/Register/RegisterUserAccountUseCaseTests.cs
@@ -0,0 +1,38 @@
+﻿using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Security;
+using MyRecipeBook.Application.UseCases.User.Register;
+using Shouldly;
+
+namespace UseCases.Tests.User.Register;
+
+public class RegisterUserAccountUseCaseTests 
+{
+    [Fact]
+    public async Task Success()
+    {
+        //AAA
+
+        //Arrange
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        var useCase = CreateUseCase();
+
+        //Act
+        var result = await useCase.Execute(request);
+
+        //Assert
+        result.ShouldNotBeNull();
+        result.Tokens.ShouldNotBeNull();
+        result.Name.ShouldBe(request.Name);
+    }
+
+    private RegisterUserAccountUseCase CreateUseCase()
+    {
+        var unitOfWork = IUnitOfWorkBuilder.Builder();
+        var userWriteOnlyRepository = IUserWriteOnlyRepositoryBuilder.Builder();
+        var userReadOnlyRepository = new IUserReadOnlyRepositoryBuilder().Builder();
+        var passwordHasher = new IPasswordHasherBuilder().Builder();
+
+        return new RegisterUserAccountUseCase(passwordHasher, userWriteOnlyRepository, userReadOnlyRepository, unitOfWork);
+    }
+}
