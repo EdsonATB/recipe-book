573bdc982dd24ec58e84fe4d2a8cd0d54b0a0eda
Edson <edsonaugusto2006@gmail.com>
2026-09-24 17:02:05 -0300

    Set up initial useCase for login + add it to dependency injection

--- src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Application.UseCases.User.Login.WithEmailAndPassword;
 using MyRecipeBook.Application.UseCases.User.Register;
 
 namespace MyRecipeBook.Application;
@@ -8,5 +9,6 @@ public static class DependencyInjectionExtension
     public static void AddApplication(this IServiceCollection services) //o this pega o objeto que esta chamando essa funçao
     {
         services.AddScoped<IRegisterUserAccountUseCase, RegisterUserAccountUseCase>();
+        services.AddScoped<ILoginWithEmailAndPasswordUseCase, LoginWithEmailAndPasswordUseCase>();
     }
 }
