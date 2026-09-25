86ea09b66f03615f9acd6fddb06f30b83490630c
Edson <edsonaugusto2006@gmail.com>
2026-09-25 19:03:23 -0300

    Create abstract methods for parent exception class + implement methods on child classes

--- src/Shared/MyRecipeBook.Exception/ExceptionsBase/ErrorOnValidationException.cs
+++ src/Shared/MyRecipeBook.Exception/ExceptionsBase/ErrorOnValidationException.cs
@@ -1,11 +1,15 @@
-﻿namespace MyRecipeBook.Exception.ExceptionsBase;
+﻿using System.Net;
+
+namespace MyRecipeBook.Exception.ExceptionsBase;
 
 public class ErrorOnValidationException : MyRecipeBookException
 {
     private readonly List<string> _errors; //recomendacao da microsoft comecar atributos que forem private readonly com _ 
     
+    
     public ErrorOnValidationException(List<string> errorMessages) => _errors = errorMessages;
 
+    public override List<string> getErrorMessages() => _errors; //return
 
-    public List<string> getErrorMessages() => _errors; //return
+    public override HttpStatusCode getStatusCode() => HttpStatusCode.BadRequest;
 }
--- src/Shared/MyRecipeBook.Exception/ExceptionsBase/MyRecipeBookException.cs
+++ src/Shared/MyRecipeBook.Exception/ExceptionsBase/MyRecipeBookException.cs
@@ -1,6 +1,9 @@
-﻿namespace MyRecipeBook.Exception.ExceptionsBase;
+﻿using System.Net;
+
+namespace MyRecipeBook.Exception.ExceptionsBase;
 
 public abstract class MyRecipeBookException : System.Exception
 {
-
+    public abstract HttpStatusCode getStatusCode();
+    public abstract List<string> getErrorMessages();
 }
