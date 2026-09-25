ba3bfd69e67a6a87e2d093a599d6dd2c4687113d
Edson <edsonaugusto2006@gmail.com>
2026-09-25 19:02:20 -0300

    Create InvalidLoginException for unauthorized login attempts + validation on useCase

--- src/Shared/MyRecipeBook.Communication/Responses/ResponseErrorJson.cs
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseErrorJson.cs
@@ -1,6 +1,6 @@
 ﻿namespace MyRecipeBook.Communication.Responses;
 
-public class ResponseErrorJson
+public class ResponseErrorJson //é usado para organizar o retorno da API, é usado com new entao no retorno vai ter as propriedades dessa classe organizadas, caso retornasse o getErrors de outra classe de exceçao da camada de exception so viria uma lista desorganizada e "sem possibilidade" de adicionar novas propriedades.
 {
     public List<string> Errors { get; private set; }
 
---
+++ src/Shared/MyRecipeBook.Exception/ExceptionsBase/InvalidLoginException.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace MyRecipeBook.Exception.ExceptionsBase;
+
+public class InvalidLoginException : MyRecipeBookException
+{
+    public override List<string> getErrorMessages() => [ResourceMessagesException.VALIDATION_LOGIN_INVALID]; // [] transforma em lista
+
+    public override HttpStatusCode getStatusCode() => HttpStatusCode.Unauthorized;
+}
